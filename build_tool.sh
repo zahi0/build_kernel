d666bc08da67380f3c7ab63b6e3642df00ab725f
timiiify <64126096+timiiify@users.noreply.github.com>
2023-08-05 09:18:49 +0800

    Update build_tool.sh

--- build_tool.sh
+++ build_tool.sh
@@ -48,7 +48,7 @@ clean(){
 build_kernel(){
 	export KBUILD_BUILD_USER="zahi0"  
 	export KBUILD_BUILD_HOST="zahi0-server"  
-	make $args wayne_defconfig #修改配置文件
+	make $args cepheus_user_defconfig #修改配置文件
 	if [ $? -ne 0 ]; then
       		exit 0
         fi
